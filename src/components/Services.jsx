09ab26e2d4f5a855aeede7d8e3ff842c1ee041a0
wanda <rashiidahweira@gmail.com>
2023-12-15 15:09:22 +0300

    Services adjusted

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Services = ({services}) => {
- 
+const Services = ({ services }) => {
   return (
     <div className="flex flex-col items-center mt-28">
       <h1 className="text-4xl font-bold mt-5">Our Services</h1>
@@ -16,24 +15,24 @@ const Services = ({services}) => {
             key={index}
             className="p-2 shadow-xl border rounded-xl w-52 flex flex-col items-center bg-gray-50"
           >
-            <img
-              src={service.imageUrl}
-              alt={service.name}
-              className="w-20 rounded-full"
-            />
-            <p className="p-2 text-sm text-center font-semibold">
-              {service.name}
-            </p>
-            <p className="text-xs p-2">
-              {service.description.length > 120
-                ? `${service.description.substring(0, 120)}...`
-                : service.description}
-            </p>
-            {service.description.length > 120 && (
-              <Link to={`/services/${index}`}>
-                <p className="text-sky-500 my-4">Read more</p>
-              </Link>
-            )}
+            <Link to={`/services/${index}`}>
+              <img
+                src={service.imageUrl}
+                alt={service.name}
+                className="w-20 rounded-full"
+              />
+              <p className="p-2 text-sm text-center font-semibold">
+                {service.name}
+              </p>
+              <p className="text-xs p-2 text-center">
+                {service.description.length > 120
+                  ? `${service.description.substring(0, 120)}...`
+                  : service.description}
+              </p>
+              {service.description.length > 120 && (
+                <p className="text-sky-500 my-4 text-center">Read more</p>
+              )}
+            </Link>
           </div>
         ))}
       </div>
